de473259add9da9ea9c6ebeeb9bcc1f3b8550ac2
Tyler <recondemon@outlook.com>
2024-07-18 16:05:16 -0600

    fixed build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,30 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     eslint({
       lintOnStart: true,
-      failOnError: mode === 'production',
-    }),
+      failOnError: mode === "production"
+    })
   ],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: [
-        '/dist/assets/index-0cfd6ba9.js',
-        '/dist/assets/index-dbc314b5.css',
-      ],
-    },
-  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:8000'
     },
-  },
+    open: true
+  }
 }));
